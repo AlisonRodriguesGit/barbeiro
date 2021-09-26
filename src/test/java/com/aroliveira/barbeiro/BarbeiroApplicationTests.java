ba3f8002bbc2d6bc9648396b5834cf00863e46f1
Alison Rodrigues <alison.aro@gmail.com>
2021-09-26 12:44:02 -0300

    Inicio do Projeto com Spring Web

---
+++ src/test/java/com/aroliveira/barbeiro/BarbeiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aroliveira.barbeiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbeiroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
